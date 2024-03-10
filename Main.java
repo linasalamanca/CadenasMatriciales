4994246a731fae84aadec95b07f4987b7bf997d3
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2024-03-10 10:56:43 -0500

    Inicio Reverso

--- Main.java
+++ Main.java
@@ -25,7 +25,45 @@ public class Main {
             }
         }
 
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        minMultReverso(d, n, M);
+
         return M[1][n];
+    }
+
+    public static int[][] minMultReverso(int[] d, int n, int m[][]) {
+        int resp[][] = new int[n+1][n+1]; 
+        int r = m[1][n];
+        System.out.println("n es: "+n);
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                resp[i][j] = 0;
+            }
+        }
+
+        for(int i=n+1; i>=0; i--){
+            for(int j=i; j<n+1; j++){
+                if(i==1 || j==n){
+                    resp[i][j] = m[i][j];
+                }
+            }
+        }
+
+        for(int i=0; i<n+1; i++){
+            for(int j=0; j<n+1; j++){
+                System.out.print(resp[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return resp;
     }
 
     public static void main(String[] args) {
