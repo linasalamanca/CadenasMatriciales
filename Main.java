e336b1e90d1cf02e4bd0044b6b85f8e2788e87f7
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2024-03-12 17:35:48 -0500

    finaaaaaal

--- Main.java
+++ Main.java
@@ -1,74 +1,63 @@
+import java.util.ArrayList;
+
 public class Main {
-    public static int minMultBottomUp(int[] d) {
-        int n = d.length - 1;
-        int[][] M = new int[n + 1][n + 1];
+    public static class MatrixChainMultiplication {
 
-        // Inicializar con valores máximos excepto la diagonal principal
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n; j++) {
-                M[i][j] = Integer.MAX_VALUE;
+        public static int minMultBottomUp(int[] d) {
+            int n = d.length - 1;
+            int[][] M = new int[n + 1][n + 1];
+            int[][] S = new int[n + 1][n + 1];
+
+            for (int i = 1; i <= n; i++) {
+                M[i][i] = 0;
             }
-        }
 
-        // Inicializar la diagonal principal de M
-        for (int i = 1; i <= n; i++) {
-            M[i][i] = 0;
-        }
-
-        // Calcular M[i][j] para todas las subsecuencias de longitud l
-        for (int l = 2; l <= n; l++) {
-            for (int i = 1; i <= n - l + 1; i++) {
-                int j = i + l - 1;
-                for (int k = i; k < j; k++) {
-                    M[i][j] = Math.min(M[i][j], M[i][k] + M[k + 1][j] + d[i - 1] * d[k] * d[j]);
+            for (int l = 2; l <= n; l++) {
+                for (int i = 1; i <= n - l + 1; i++) {
+                    int j = i + l - 1;
+                    M[i][j] = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        int q = M[i][k] + M[k + 1][j] + d[i - 1] * d[k] * d[j];
+                        if (q < M[i][j]) {
+                            M[i][j] = q;
+                            S[i][j] = k;
+                        }
+                    }
                 }
             }
+
+            reconstruirPasos(S, n);
+            return M[1][n];
         }
 
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<n+1; j++){
-                System.out.print(M[i][j] + " ");
-            }
-            System.out.println();
-        }
+        /*
+         * private static void reconstruirPasos(int[][] S, int i, int j) {
+         * if (i < j) {
+         * System.out.println("Multiplica la cadena de matrices de " + i + " a " +
+         * S[i][j] + " y de " + (S[i][j] + 1) + " a " + j);
+         * reconstruirPasos(S, i, S[i][j]);
+         * reconstruirPasos(S, S[i][j] + 1, j);
+         * }
+         * }
+         */
 
-        minMultReverso(d, n, M);
-
-        return M[1][n];
-    }
-
-    public static int[][] minMultReverso(int[] d, int n, int m[][]) {
-        int resp[][] = new int[n+1][n+1]; 
-        int r = m[1][n];
-        System.out.println("n es: "+n);
-
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<n+1; j++){
-                resp[i][j] = 0;
-            }
-        }
-
-        for(int i=n+1; i>=0; i--){
-            for(int j=i; j<n+1; j++){
-                if(i==1 || j==n){
-                    resp[i][j] = m[i][j];
+        private static void reconstruirPasos(int[][] S, int n) {
+            ArrayList<Integer> resp = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (S[i][j] != 0) {
+                        resp.add(S[i][j]);
+                    }
                 }
             }
+
+            System.out.println(resp);
         }
 
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<n+1; j++){
-                System.out.print(resp[i][j] + " ");
-            }
-            System.out.println();
+        public static void main(String[] args) {
+            int[] d = { 2, 3, 4, 5, 3 };
+            int minMultiplicaciones = minMultBottomUp(d);
+            System.out.println("Cantidad mínima de multiplicaciones: " + minMultiplicaciones);
         }
-
-        return resp;
-    }
-
-    public static void main(String[] args) {
-        int[] d = {2, 3, 4, 5, 3};
-        int minMultiplicaciones = minMultBottomUp(d);
-        System.out.println("Cantidad mínima de multiplicaciones: " + minMultiplicaciones);
     }
 }
